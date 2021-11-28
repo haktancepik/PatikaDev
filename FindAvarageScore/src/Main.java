e3f353f528255d3793af388a65728a0e1e666c7d
haktancepik <haktan.cepik@gmail.com>
2021-11-28 21:14:57 +0300

    Find Average Score
    The console app find the average of course score

---
+++ FindAvarageScore/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    // read the input from user
+        // take the average of inputs
+        // print average to console
+
+
+    }
+}
